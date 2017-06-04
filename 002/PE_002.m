c2fe29c96d91be2137b621ad2952fa1e63958d28
Miraç Vuslat Başaran <miracvbasaran@gmail.com>
2017-06-04 12:33:52 -0400

    Solution in MatLab

---
+++ 002/PE_002.m
@@ -0,0 +1,18 @@
+function sum = PE_002
+limit = 4000000; % limit is four million
+sum = 0;
+
+a = 1;
+b = 1;
+
+% finding the fibonacci numbers
+while b < limit
+    if mod(b,2) == 0
+        sum = sum + b;
+    end
+    temp = b;
+    b = a + b;
+    a = temp;
+end
+
+end
